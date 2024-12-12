a8d63afe40d89928c7382c66c7078128fc34b968
AndresDiaz99 <155788017+AndresDiaz99@users.noreply.github.com>
2024-12-12 20:32:07 +0000

    Hello World/Jump lines

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,10 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+# include <iostream> //do not end with ";"
+
+int main(){
+    std::cout <<"Hi there!\n"; // jump a line with \n
+    std::cout <<"woowoowoo" << std::endl; //jump a line with "endl"
+    return 0;
+}
