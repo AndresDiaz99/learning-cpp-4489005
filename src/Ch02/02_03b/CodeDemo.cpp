884e2e7985e9134b4b8288d1107e60d788c41564
AndresDiaz99 <155788017+AndresDiaz99@users.noreply.github.com>
2024-12-13 03:31:14 +0000

    Add comments/Unisgned issues

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,9 +4,33 @@
 
 #include <iostream>
 
+int a,b=5; /*at the same level of indentation as int main() so this are GLOBAL VARIABLES. Accessible to all parts of the code
+Managed statically by the complier
+Allocated in the DATA segment of memory
+Global Variables are not generally recommended
+*/
+
 int main(){
-    std::cout << "Hi There!" << std::endl;
-    
+    /*Local Variables to the scope
+    inside functions or loops, only accessible within the scope
+    aka: Automatic variables, because are managed by the compiler
+    allocated in the stack segment of memory - Temporary */
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+
+    std::cout << "a= " << a << std::endl;
+    std::cout << "b= " << b << std::endl;
+    std::cout << "flag= " << my_flag << std::endl;
+    my_flag = true;
+    std::cout << "flag= " << my_flag << std::endl;
+    std::cout << "a + b= " << a +b << std::endl;
+    std::cout << "b - a= " << b - a << std::endl;
+
+    unsigned int positive;
+    positive = b - a;
+    std::cout << "b - a (unisgned)= " << positive << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
