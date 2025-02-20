c8ccafe72fafab88d684aa58adc21361f85271b5
AndresDiaz99 <155788017+AndresDiaz99@users.noreply.github.com>
2025-02-20 21:27:28 +0000

    Some more changes lol

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -9,7 +9,7 @@
 int main(){
     std::string str;
     std::cout << "Nombre?\n" << std::flush; //flush to wait for the whole message to be displayed before accepting the input
-    std::cin >> str;
+    std::cin >> str; //Only works for single words without spaces
     std::cout << "Hola, " << str << ", qué cuentas?" << std::endl;
 
     std::cout << std::endl << std::endl;
--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -1,11 +1,24 @@
 // Learning C++ 
-// Exercise 02_06
+// Exercise 02_06 MODIFIED BY ANDRES
 // Preprocessor directives, by Eduardo Corpeño 
 
-#include <iostream>
+#include <iostream> //<> means the preprocessor will look for the file in a predefined location
+#include <string>
+#include <cstdint> //provides fix lenght definitions for int types so you dont have to depend
+            // on the implementation of primitive types like "int" or "char"
+
+#define CAPACITY 5000
+#define DEBUG //If commented the ifdef DEBUG part will not be executed
 
 int main(){
-    
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+#ifdef DEBUG
+    std::cout << "About to perform the addition" << std::endl;
+#endif
+    large += small;//large = large + small;
+    std::cout << "the large interger is " << large << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
