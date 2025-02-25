4db3cd2f624fbc01e2e2a2d7e528ff2d0033ca9c
AndresDiaz99 <155788017+AndresDiaz99@users.noreply.github.com>
2025-02-25 21:17:37 +0000

    strings_mods and notes

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -1,11 +1,30 @@
 // Learning C++ 
-// Exercise 02_09
+// Exercise 02_09 MODIFICADO POR ANDRES
 // Strings, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstring> //to include part of the C standard library
 
 int main(){
-    
+//two char arrays as in C
+    const size_t LENGTH = 25;
+    char array_str1[LENGTH] = "Hey guys! "; //this constant string means a sequence of ASCII characters terminated by 0
+    char array_str2[]= "What's up?";
+
+//two string objects as in C++
+    std::string std_str1 = "Hi everybody! ";
+    std::string std_str2 = "How's it going?";
+
+//for the char variables the <cstring> provides the strcat family of functions
+    strncat (array_str1, array_str2, LENGTH); //this modifies the first string
+    //strncat (destination_string, second array to concatenate, max length to produce)
+    std::cout << array_str1 << std::endl;
+// for the <string> class
+    std::cout << std_str1 + std_str2 << std::endl; //non of the operants is modified
+
     std::cout << std::endl << std::endl;
     return (0);
 }
+/*Not usual to resort to the <cstring> class, is generally better to use <string> class
+Usually the <cstring> class is used when the code needs to be compatible with existing C code*/
