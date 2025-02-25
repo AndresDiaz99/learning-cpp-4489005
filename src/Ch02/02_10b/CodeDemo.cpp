20953beb9d61cb9168c1719adff8cdab6c41706a
AndresDiaz99 <155788017+AndresDiaz99@users.noreply.github.com>
2025-02-25 22:19:23 +0000

    implicit and explicit cast

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -1,12 +1,24 @@
 // Learning C++ 
-// Exercise 02_10
+// Exercise 02_10 MODIFICADO POR ANDRES
 // Type Casting, by Eduardo Corpeño 
 
 #include <iostream>
 #include <cstdint>
 
 int main(){
-    
+    float flt;
+    int32_t sgn;
+    uint32_t unsgn;
+
+    flt = -7.66; //Implicit casting. No 'f' characher at the end so it is a 'double' constant
+    sgn = flt; //double or float to int32_t gets truncated
+    unsgn = sgn; //the 32 bits version of the 2's complement of 7 -> (2^32) - 7
+
+    std::cout << "float " << flt << std::endl;
+    std::cout << "int32_t " << sgn << std::endl;
+    std::cout << "unint32_t "<< unsgn << std::endl;
+    std::cout << "unint32_t "<< (int32_t) unsgn << std::endl; //Explicit cast
+
     std::cout << std::endl << std::endl;
     return (0);
 }
