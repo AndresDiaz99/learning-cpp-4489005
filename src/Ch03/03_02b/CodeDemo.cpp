4499ec6a24b655787094c5837eefcb9623d3024c
AndresDiaz99 <155788017+AndresDiaz99@users.noreply.github.com>
2025-02-26 22:39:26 +0000

    Classes_getters_NOTES

--- src/Ch03/03_02b/CodeDemo.cpp
+++ src/Ch03/03_02b/CodeDemo.cpp
@@ -7,7 +7,29 @@
 
 enum class cow_purpose {dairy, meat, hide, pet};
 
-struct cow{
+class cow{
+public: // without this the class will protect the data and will be innaccesible from other parts of the code
+    /*Constructor
+    The constructor of a class is called when an object is created.
+    Always implemented.
+    Simply creates the object that allocates the memory for the data members*/
+    cow(std::string name_i, int age_i, cow_purpose purpose_i){
+        name = name_i;
+        age= age_i;
+        purpose = purpose_i;
+        /*Additional versions of the constructor can be written by taking a custom set of parameters
+        AKA "overloading a function" -> Defining a function with the same name as an already defined function but with different set of parameters*/
+    }
+    std::string get_name() const{ //FUNCTION. specify it as const is a BEST PRACTICE
+        return name;
+    }
+    int get_age() const{ // getters does not modify anything of the object they get data from
+        return age; //only return
+    }
+    cow_purpose get_purpose() const{
+        return purpose;
+    }
+private:
     std::string name;
     int age;
     cow_purpose purpose;
