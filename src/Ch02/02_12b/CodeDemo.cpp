f205d256db71bc7489c8f775f444f5a31b371301
AndresDiaz99 <155788017+AndresDiaz99@users.noreply.github.com>
2025-02-26 00:02:26 +0000

    Enum_cpp

--- src/Ch02/02_12b/CodeDemo.cpp
+++ src/Ch02/02_12b/CodeDemo.cpp
@@ -1,18 +1,36 @@
 // Learning C++ 
-// Exercise 02_12
+// Exercise 02_12 MODIFICADO POR ANDRES
 // Enumerations, by Eduardo Corpeño 
 
 #include <iostream>
 #include <cstdint>
 
-enum cow_purpose {dairy, meat, hide, pet};
+#define CLNG
+#define CPP
+
+// enum cow_purpose {dairy, meat, hide, pet};
+// enum grocery_section {canned, frozen, meat, laundry, dairy, bakery}; --> this is an error
+/*A bad solution would be to change the names for meat and dairy*/
+
+//is better to do strong typed enums
+enum class cow_purpose {dairy, meat, hide, pet};
+enum class grocery_section {canned, frozen, meat, laundry, dairy, bakery};
 
 int main(){
-    int a;
+#ifdef CLNG
+    cow_purpose a;
+    int meat = 8; //this will overwrite the 'enum' value and set a = 8
 
-    a = meat;
+    // a = cow_purpose::meat; --> throws an error if 'a' is of type 'int', it needs to be of type cow_purpose
+    a = cow_purpose::meat;
 
-    std::cout << "a = " << a << std::endl;
+    // std::cout << "a = " << a << std::endl; --> error, 'cout' is not defined for the specific class
+    std::cout << "a = " << (int) a << std::endl;
+#endif
+
+#ifdef CPP
+    // commented section is using C enum
+#endif
 
     std::cout << std::endl << std::endl;
     return (0);
